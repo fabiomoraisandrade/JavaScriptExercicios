5786b08ea8479001d3a2b183f827a43f0ec5efbd
Fábio Morais de Andrade <fabio.adr12@gmail.com>
2022-11-30 20:08:20 -0300

    Exercicio 4

---
+++ ex004.js
@@ -0,0 +1,5 @@
+const readline = require('readline-sync');
+
+let n = readline.question('Digite algo: ')
+console.log(`O tipo é: ${typeof(n)}`);
+//O tipo do método question do readline-sync sempre será uma string
